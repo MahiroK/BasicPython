f5d93e4d88d0fc3d96ad3ea6102f8568aec5e1db
MahiroK <a1r6j37it@outlook.jp>
2024-03-27 23:36:05 +0900

    7-2　Py.py

--- pi.py
+++ pi.py
@@ -1,7 +1,10 @@
-text = """
+txt = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
+result = list(map(len, ["""How I want a drink, alcoholic of course, after the heavy chapters involving
+quantum mechanics All of thy geometry, Herr Planck, is fairly hard """]))
+
+print[result]
 
