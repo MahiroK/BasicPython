7b3522694492584199cc33a78bbef5ae9558f60c
MahiroK <a1r6j37it@outlook.jp>
2024-03-30 11:04:26 +0900

    2024/3/30 変更しました

--- pi.py
+++ pi.py
@@ -3,8 +3,8 @@ txt = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-result = list(map(len, ["""How I want a drink, alcoholic of course, after the heavy chapters involving
-quantum mechanics All of thy geometry, Herr Planck, is fairly hard """]))
+lengths = list(map(len, ["How","I","want","a","drink","alcoholic","of","course","after","the","heavy","chapters","involving"
+"quantum","mechanics","All","of","thy","geometry","Herr","Planck","is","fairly","hard"]))
 
-print[result]
+print[lengths]
 
