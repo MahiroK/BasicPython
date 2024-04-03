0136bbe1a0025b82bd561e629dd98613c25a36e9
MahiroK <a1r6j37it@outlook.jp>
2024-04-04 00:14:46 +0900

    2024年4月4日　python入門課題

--- pi.py
+++ pi.py
@@ -2,6 +2,7 @@ txt = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
+#文字列のカウント
 py = list(map(len,txt.replace('.', '').replace(',', '').split()))
 print(py)
 answer = ""
