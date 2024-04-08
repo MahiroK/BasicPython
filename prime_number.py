bb7cf16d3c455e17744f08fd81b93207cdf06cf4
MahiroK <a1r6j37it@outlook.jp>
2024-04-08 10:55:16 +0900

    2024年4月8日　制御構文

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,17 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+# 自然数nが与えられたとき、素数であるか素数でないかを判定する問題
+#(1) 61が素数であること
+#(2) 10が素数でないこと
 
-# TODO
+n = int(input("数値を入力してください: "))
+
+def is_prime(n):
+
+    #　if文・for文を用いて正誤判定をしました
+    for i in range (2,n):
+     if n % i == 0:
+        return "素数ではありません"
+    return "素数と判定されました"
+    
+     
+result = is_prime(n)
+print(result)    
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,25 @@
-from math import sin
+from math import sin,pi
 # --example--
+a = int('0')
+b = pi/int('2')
+n = int('100')
+
+# 短冊の幅Δx
+dx = (b-a)/n
+
+# 面積の総和
+s = 0
+for i in range(1,100):
+    x1 = a + dx*i
+    x2 = a + dx*(i+1)
+    f1 = sin(x1)    # 上底
+    f2 = sin(x2)    # 下底
+    # 面積
+    s += dx * (f1+f2)/2
+
+# 結果の表示(小数点以下10桁)
+print("{:.10f}".format(s))
+
 # print(sin(0))
 # >>> 0
 # -----------
