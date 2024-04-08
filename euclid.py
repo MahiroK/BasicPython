989cf8f08dd54dd67cb0e4cb32c75bd8196b7e69
MahiroK <a1r6j37it@outlook.jp>
2024-04-08 11:03:03 +0900

    2024年4月8日　制御構文

--- euclid.py
+++ euclid.py
@@ -1,4 +1,16 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+x = input('数値を入力してください')
+y = input('数値を入力してください')
 
-# TODO
+#ユークリッドの互除法
+def gcd(x, y):
+   r = x % y
+   while r != 0:
+    return y
+
+if __name__ == '__main__':
+    num1 = x
+    num2 = y
+
+    GCD = gcd(x, y)
+
+    print(x, y)  ("gcd: {GCD}")
--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,7 @@
-# TODO
+# マシンイプシロン
+EPS = 0.0
+tmp = 1.0
+while 1.0 + tmp > 1.0:
+  EPS = tmp
+  tmp /= 2
+print(EPS)
