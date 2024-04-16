a6dc6aeabdcb8e139e47566dc126dc76f7aaf53e
MahiroK <a1r6j37it@outlook.jp>
2024-04-16 22:25:02 +0900

    2024年4月16日　変更しました

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -5,18 +5,21 @@ b = pi/int('2')
 n = int('100')
 
 # 短冊の幅Δx
-dx = (b-a)/n
+h = (b-a)/n
+
 
 # 面積の総和
 s = 0
-# 4/10 変更：　range内を（1,101）に変更しました
-for i in range(1,101):
-    x1 = a + dx*i
-    x2 = a + dx*(i+1)
+# 4/16 変更：　range内を（1,n+1）に変更しました
+# 問題文の式をもとに修正してみました。
+
+for i in range(1,n+1):
+    x1 = a+h*(i-1)
+    x2 = a+h*i
     f1 = sin(x1)    # 上底
     f2 = sin(x2)    # 下底
     # 面積
-    s += dx * (f1+f2)/2
+    s += h * (f1+f2)/2
 
 # 結果の表示(小数点以下10桁)
 print("{:.10f}".format(s))
